ad377eddcbf45d5b2cc0a7056e0b8cad3bb10b94
madhudskumar <madhudskumar@live.in>
2016-09-11 04:30:48 +0530

    added watchers

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,18 @@ var gulp = require('gulp'),
     npm = require('./package.json'),
     prerest = require('./library/prerest')();
 
+const claOpt = [
+    { name: 'src', type: String, alias:'s', multiple: false, defaultOption: false },
+    { name: 'multiple', alias:'m', type: String },
+    { name: 'help', alias: 'h', type: Boolean },
+    { name: 'port', alias: 'p', type: Number },
+    { name: 'version', alias:'v', type: Boolean }
+];
+
+var opt = cla(claOpt),
+    port = 0;
+
 gulp.task('serve', function () {
-    const claOpt = [
-        { name: 'src', type: String, alias:'s', multiple: false, defaultOption: false },
-        { name: 'multiple', alias:'m', type: String },
-        { name: 'help', alias: 'h', type: Boolean },
-        { name: 'port', alias: 'p', type: Number },
-        { name: 'version', alias:'v', type: Boolean }
-    ];
-
-    var opt = cla(claOpt);
-
     if(opt.version && !opt.help) console.log(npm.version);
     else if(opt.help && !opt.version){
         console.log("\t" + chalk.white.bold('HELP :'));
@@ -23,17 +24,19 @@ gulp.task('serve', function () {
         console.log("\t--help|-h help");
         console.log("\t--version|-v versionNumber");
     }else if(opt.src || opt.multiple){
-        if (opt.src && !opt.multiple) prerest.createPath(opt.src);
-        else {
+        if (opt.src && !opt.multiple) {
+            prerest.createPath(opt.src);
+            gulp.watch(opt.src, ['serve']);
+        }else {
             var urlConstructor = require('./' + opt.multiple.toString().trim());
             urlConstructor.forEach(function (url) {
                 prerest.createPath(url.src, url.path);
             })
         }
-        prerest.createServer(opt.port);
+        if(!port++) prerest.createServer(opt.port);
     }else if(opt.multiple){
 
     }else if(!(opt.src || opt.help || opt.version || opt.multiple)){
         console.log('Not a valid option. --help for more');
     }
-})
+});
